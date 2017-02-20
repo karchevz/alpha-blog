e8de4b723334e2d3d47f5d9f1e93f558d38f59c8
karchevz <zheko.karchev@gmail.com>
2017-02-20 18:40:32 -0500

    Added pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,8 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -15,7 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    debugger
+    #debugger
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     @article.user = User.first
