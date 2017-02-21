b6560da9175abfae658f3dde531de4b290579a14
karchevz <zheko.karchev@gmail.com>
2017-02-21 13:46:43 -0500

    Added login link

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,9 @@ Rails.application.routes.draw do
 
   resources :users, except:[:new]
 
-
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
