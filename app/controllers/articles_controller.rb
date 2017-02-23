78e300bcfe5f24b449d3bbf0d7c6ce93e9fe1987
karchevz <zheko.karchev@gmail.com>
2017-02-22 21:14:00 -0500

    Completed security and access control actions at model and UI level

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
@@ -17,7 +19,7 @@ class ArticlesController < ApplicationController
     #debugger
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     #@article.save
     #redirect_to article_path(@article)
     if @article.save
@@ -53,6 +55,13 @@ class ArticlesController < ApplicationController
 
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit or delete your own articles"
+        redirect_to root_path
+      end
     end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  
+  before_action :require_same_user,  only: [:edit, :update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end 
@@ -40,8 +41,15 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-end
 
   def set_user
     @user = User.find(params[:id])
   end
+
+  def require_same_user
+    if current_user != @user
+        flash[:danger] = "You can only endit your own account"
+        redirect_to rooth_path
+    end
+  end
+end
